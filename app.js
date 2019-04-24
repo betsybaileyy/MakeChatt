7d4f3a610a72494cc10abbc77b72de37cb5e2f89
betsybaileyy <betsy.bailey80@gmail.com>
2019-04-24 15:03:13 -0700

    integrated sockett

---
+++ app.js
@@ -0,0 +1,27 @@
+//App.js
+const express = require('express');
+const app = express();
+//Socket.io has to use the http server
+const server = require('http').Server(app);
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
+//Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars');
+})
+
+//Establish public folder
+app.use('/public', express.static('public'))
+
+server.listen('9000', () => {
+  console.log('Server listening on Port 9000');
+})
